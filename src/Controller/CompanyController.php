3f3a512662399e2e7350db9d9b860fcda11d5794
naimdeb <naimdebard02@gmail.com>
2025-02-05 16:41:32 +0100

    separated controllers

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -6,18 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class CompanyController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/company', name: 'app_company')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('company.html.twig', [
+            'controller_name' => 'CompanyController',
         ]);
     }
-
-
-
 }
-
-
-
